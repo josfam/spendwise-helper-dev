0380bddf00ae499610146822b2cd08db6b73aa8c
josfam <jamukun@proton.me>
2024-06-24 18:42:07 +0300

    Add useful message about the running environment

--- run_app_in_dev_env.sh
+++ run_app_in_dev_env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # starts the applicaiton in the development environment (tables persist)
+echo "Running in development environment (all changes are saved)"
+
 SPENDWISE_ENV=dev \
 SPENDWISE_MYSQL_USER=spendwise_test_user \
 SPENDWISE_MYSQL_PWD='' \
